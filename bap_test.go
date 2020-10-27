e3a76bcf50dff5a99fe88cbc06321ddae347a64a
mrz1836 <mrz1818@gmail.com>
2020-10-27 10:54:57 -0400

    Added tests and examples for CreateIdentity

--- bap_test.go
+++ bap_test.go
@@ -1,6 +1,7 @@
 package bap
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/libsv/libsv/transaction"
@@ -12,18 +13,108 @@ const idKey = "8bafa4ca97d770276253585cb2a49da1775ec7aeed3178e346c8c1b55eaf5ca2"
 
 // TestCreateIdentity will test the method CreateIdentity()
 func TestCreateIdentity(t *testing.T) {
+
+	t.Parallel()
+
+	var (
+		// Testing private methods
+		tests = []struct {
+			inputPrivateKey string
+			inputIDKey      string
+			inputCounter    uint32
+			expectedTxID    string
+			expectedNil     bool
+			expectedError   bool
+		}{
+			{
+				privateKey,
+				idKey,
+				0,
+				"49957864306b123c3cca8711635ba88890bb334eb3e9f21553b118eb4d66cc62",
+				false,
+				false,
+			},
+			{
+				"",
+				idKey,
+				0,
+				"49957864306b123c3cca8711635ba88890bb334eb3e9f21553b118eb4d66cc62",
+				true,
+				true,
+			},
+			{
+				"invalid-key",
+				idKey,
+				0,
+				"49957864306b123c3cca8711635ba88890bb334eb3e9f21553b118eb4d66cc62",
+				true,
+				true,
+			},
+			{
+				privateKey,
+				"",
+				0,
+				"49957864306b123c3cca8711635ba88890bb334eb3e9f21553b118eb4d66cc62",
+				true,
+				true,
+			},
+			{
+				privateKey,
+				idKey,
+				1,
+				"d820499a7fb3561d91d71d4eb8de636ae3bb1b7eca97497d7b6fbc3b164ea5b1",
+				false,
+				false,
+			},
+			{
+				privateKey,
+				idKey,
+				100,
+				"e0c569310c5066dbda4ccdf25c2c7591f2dcb246528d2763ca5167b0f37d71b4",
+				false,
+				false,
+			},
+		}
+	)
+
+	// Run tests
+	for _, test := range tests {
+		if tx, err := CreateIdentity(test.inputPrivateKey, test.inputIDKey, test.inputCounter); err != nil && !test.expectedError {
+			t.Errorf("%s Failed: [%s] [%s] [%d] inputted and error not expected but got: %s", t.Name(), test.inputPrivateKey, test.inputIDKey, test.inputCounter, err.Error())
+		} else if err == nil && test.expectedError {
+			t.Errorf("%s Failed: [%s] [%s] [%d] inputted and error was expected", t.Name(), test.inputPrivateKey, test.inputIDKey, test.inputCounter)
+		} else if tx == nil && !test.expectedNil {
+			t.Errorf("%s Failed: [%s] [%s] [%d] inputted and nil was not expected", t.Name(), test.inputPrivateKey, test.inputIDKey, test.inputCounter)
+		} else if tx != nil && test.expectedNil {
+			t.Errorf("%s Failed: [%s] [%s] [%d] inputted and nil was expected", t.Name(), test.inputPrivateKey, test.inputIDKey, test.inputCounter)
+		} else if tx != nil && tx.GetTxID() != test.expectedTxID {
+			t.Errorf("%s Failed: [%s] [%s] [%d] inputted and expected [%s] but got [%s]", t.Name(), test.inputPrivateKey, test.inputIDKey, test.inputCounter, test.expectedTxID, tx.GetTxID())
+		}
+	}
+}
+
+// ExampleCreateIdentity example using CreateIdentity()
+func ExampleCreateIdentity() {
 	tx, err := CreateIdentity(privateKey, idKey, 0)
 	if err != nil {
-		t.Fatalf("failed to create identity: %s", err.Error())
+		fmt.Printf("failed to create identity: %s", err.Error())
+		return
 	}
 
-	if tx.GetTxID() != "e6b6aad0cd2d1c0aa0a854e7acc0e9b164e722bf041fb24c48a3998da1e1e463" {
-		t.Fatalf("failed to create identity, got: %s", tx.GetTxID())
+	fmt.Printf("tx generated: %s", tx.GetTxID())
+	// Output:tx generated: 49957864306b123c3cca8711635ba88890bb334eb3e9f21553b118eb4d66cc62
+}
+
+// BenchmarkCreateIdentity benchmarks the method CreateIdentity()
+func BenchmarkCreateIdentity(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_, _ = CreateIdentity(privateKey, idKey, 0)
 	}
 }
 
 // TestCreateAttestation will test the method CreateAttestation()
 func TestCreateAttestation(t *testing.T) {
+	t.Parallel()
 
 	// Entity / Service Provider's Identity Private Key
 	entityPk := "xprv9s21ZrQH143K3PZSwbEeXEYq74EbnfMngzAiMCZcfjzyRpUvt2vQJnaHRTZjeuEmLXeN6BzYRoFsEckfobxE9XaRzeLGfQoxzPzTRyRb6oE"
